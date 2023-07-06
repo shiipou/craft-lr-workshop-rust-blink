b2fc717bd3c2da460ec45e234d3509655dcb657c
shiipou <shiishii@nocturlab.fr>
2023-07-06 14:13:07 +0200

    feat: add interrupt on button pressed

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use core::panic::PanicInfo;
 use cortex_m_rt::entry;
-use stm32l4xx_hal::{delay::Delay, prelude::*};
+use stm32l4xx_hal::{delay::Delay, prelude::*, gpio::Edge, interrupt};
 
 use rtt_target::{rprintln, rtt_init_print};
 
@@ -22,12 +22,20 @@ fn main() -> ! {
 
   let mut gpioa = device.GPIOA.split(&mut rcc.ahb2);
   let mut gpiob = device.GPIOB.split(&mut rcc.ahb2);
+  let mut gpioc = device.GPIOC.split(&mut rcc.ahb2);
 
   let mut led1 = gpioa.pa5.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
-
   let mut led2 = gpiob.pb14.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
 
   let mut timer = Delay::new(core.SYST, clocks);
+
+  let mut userbtn = gpioc.pc13.into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
+  // Enable the interrupt on the button
+  let mut syscfg = device.SYSCFG;
+  userbtn.make_interrupt_source(&mut syscfg, &mut rcc.apb2);
+  let mut dev_exti = device.EXTI;
+  userbtn.enable_interrupt(&mut dev_exti);
+  userbtn.trigger_on_edge(&mut dev_exti, Edge::Rising);
 
   rprintln!("Hello, world!");
 
@@ -37,9 +45,29 @@ fn main() -> ! {
   loop {
     led1.toggle();
     led2.toggle();
+
     rprintln!("toggle leds");
     timer.delay_ms(1000_u32);
   }
+}
+
+#[interrupt]
+fn EXTI15_10() {
+  rprintln!("Button pressed");
+
+  let device = stm32l4xx_hal::stm32::Peripherals::take().unwrap();
+  let mut gpioc = device.GPIOC.split(&mut device.RCC.constrain().ahb2);
+  let mut led3 = gpioc.pc9.into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
+  let userbtn = gpioc.pc13.into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
+
+  // Enable the interrupt in the NVIC
+  cortex_m::interrupt::free(|cs| {
+    if userbtn.is_low() {
+      led3.set_low();
+    } else {
+      led3.set_high();
+    }
+  });
 }
 
 #[panic_handler]
